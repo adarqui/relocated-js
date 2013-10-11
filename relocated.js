884dcceb5778b8ea87312fc6a577a4d45621d0c2
Andrew Darqui <andrew.darqui@gmail.com>
2013-10-11 13:19:25 -0400

    the infancy

--- relocated.js
+++ relocated.js
@@ -1,6 +1,8 @@
 var glob = require('glob'),
     redis = require('redis'),
-    resque = require('resque')
+    resque = require('resque'),
+    async = require('async'),
+    _ = require('underscore'),
     fs = require('fs');
 
 var c
@@ -11,4 +13,43 @@ else {
     c = require('./config.js')
 }
 
-console.log(c)
+var Watcher = function(elm) {
+    var self = this
+    self.dir = elm.value
+    self.interval = 1
+    self.checker = function() { }
+    self.relocate = function() { }
+    self.glob = {}
+    self.initGlob = function() {
+        async.eachSeries(self.dir.glob, function(dir) {
+            glob(dir, {}, function(err,files) {
+                console.log(err,files)
+
+            })
+        }, function(err,data) {
+            console.log("err",err,"data",data)
+        })
+    }
+    self.initInterval = function() {
+        setInterval(function() {
+            console.log("hi")
+            self.initGlob()
+        }, self.interval*1000)
+    }
+    self.init = function() {
+        console.log(elm.value)
+//        self.initGlob()
+        if(self.dir.interval) {
+            self.interval = self.dir.interval
+        } else if(c.interval) {
+            self.interval = c.interval
+        }
+        self.initInterval()
+    }
+    self.init()
+}
+
+_.each(c.directories, function(value,key,list) {
+//    console.log(value,key,key[value],list)
+    var watcher = new Watcher({ key: key, value: value })
+})
