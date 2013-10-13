b1dda000757bd214dee999b5838578643c40382f
Andrew Darqui <andrew.darqui@gmail.com>
2013-10-12 23:06:42 -0400

    refactored stuff now working properly.

--- relocated.js
+++ relocated.js
@@ -26,7 +26,6 @@ var Watcher = function(elm) {
     self.dir = elm.value
     self.interval = 1
     self.maxproc = 0
-    self.checker = function() { return null }
     self.relocate = function() { return null }
     self.dest = null
     self.glob = {}
@@ -48,15 +47,6 @@ var Watcher = function(elm) {
                 winston.error('Relocate: '+elm.me+' success -> redis failed')
             }
             return cb()
-        })
-    }
-
-    self.redisCheckerFailed = function(elm,cb) {
-        winston.error('Checker: '+elm.me+' failed')
-        self.redis.chan.sadd(self.namespace + ':' + 'checker:failed', elm.me, function(err,dat) {
-            if(err) {
-                winston.error('Checker: '+elm.me+' failed -> redis')
-            }
         })
     }
 
@@ -94,19 +84,6 @@ var Watcher = function(elm) {
         })
     }
 
-    self.execChecker = function(elm,cb) {
-        var code = self.checker()
-        cproc.exec(code + ' ' + elm.me + ' ' + self.dir.class, function(err,stdout,stderr) {
-            if(err && err.code == 1) {
-                // we need to process this
-                return self.execRelocate(elm,cb)
-            } else {
-                // already processed
-                return self.redisCheckerFailed(elm,cb)
-            }
-        })
-    }
-
     self.processGlobFile = function(file,cb) {
         var elm
         var st
@@ -117,23 +94,11 @@ var Watcher = function(elm) {
 
             if(elm.done) return cb();
 
-            /*
-            st = fs.statSync(file)
-            if(elm.size == st.size) {
-                // file is still the same, it must be "done"
-                elm.done = true
-                return self.execChecker(elm,cb)
-            }
-
-            elm.size = st.size
-            return cb()
-            */
            fs.stat(file, function(err,data) {
                if(elm.size == data.size) {
                    elm.done = true
                    self.ev.emit('exec', { element : elm })
                    return cb()
-//                   return self.execChecker(elm,cb)
                }
                elm.size = data.size
                return cb()
@@ -150,55 +115,14 @@ var Watcher = function(elm) {
         return cb()
     }
     self.initGlob = function() {
-        /*
-        async.eachSeries(self.dir.glob, function(dir,cb) {
-            glob(dir, {}, function(err,files) {
-                if(err) return cb()
-                _.each(files, function(value,key,list) {
-                    console.log(key)
-                    if(!self.glob[key]) {
-                        return self.processGlobFile(value,cb)
-                    }
-                    else return cb()
-                })
-
-                return cb()
-            })
-        }, function(err,data) {
-            if(err) {
-                winston.error('initGlob: '+elm.key+' error', { error : err })
-            }
-            winston.info('ending loop 1')
-        })
-        */
-
-       /*
-       _.each(self.dir.glob, function(dir,key,list) {
-           glob(dir, {}, function(err, files) {
-               async.eachSeries(files, function(file, _cb) {
-                   //console.log("file", file)
-                   return _cb()
-               }, function(err, data) {
-                   winston.info('ending loop')
-               })
-           })
-       })
-       */
 
       for(var v in self.dir.glob){
           var dir = self.dir.glob[v]
           glob(dir,{},function(err,files) {
               async.forEachSeries(files,function(file,_cb) {
-                  /*
-                  process.nextTick(function() {
-                      console.log("file")
-                    return _cb()
-                  })
-                  */
                  async.setImmediate(function() {
                     return self.processGlobFile(file,_cb);
                  })
-//                  process.nextTick(_cb)
               })
           })
       }
@@ -255,16 +179,6 @@ var Watcher = function(elm) {
         } else {
             winston.error('Init: '+elm.key+' maxproc -> unspecified')
             process.exit()
-        }
-
-        if(self.dir.check) {
-            self.checker = function() { return self.dir.check }
-        } else if(c.check) {
-            self.checker = function() { return c.check }
-        }
-        else {
-            winston.error('Init: '+elm.key+' checker -> unspecified')
-            process.exit(-1)
         }
 
         if(self.dir.relocate) {
@@ -373,9 +287,6 @@ var init = {
         if(elm.dest) {
             elm.dest = misc.returnFullPath(elm.dest)
         }
-        if(elm.check) {
-            elm.check = misc.returnFullPath(elm.check)
-        }
         if(elm.relocate) {
             elm.relocate = misc.returnFullPath(elm.relocate)
         }
@@ -389,14 +300,9 @@ var init = {
             init.sanitizePathsElm(elm)
         })
         init.sanitizePathsElm(c)
-        /*
-        console.log(JSON.stringify(c,null,4))
-        process.exit()
-        */
     },
     watchers : function() {
         _.each(c.directories, function(value,key,list) {
-            //    console.log(value,key,key[value],list)
             var watcher = new Watcher({ key: key, value: value })
         })
     },
